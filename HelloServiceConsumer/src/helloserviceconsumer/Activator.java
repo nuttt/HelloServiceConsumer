321962e681a2bd2acd08c02463596abc2f1f2913
Nuttapon Pattanavijit <nuttt.p@gmail.com>
2014-04-10 16:53:17 +0900

    working

---
+++ HelloServiceConsumer/src/helloserviceconsumer/Activator.java
@@ -0,0 +1,42 @@
+package helloserviceconsumer;
+
+import ihelloservice.HelloService;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		ServiceReference[] serviceReferences = bundleContext.getAllServiceReferences(null, null);
+		for (ServiceReference serviceReference : serviceReferences) {
+			if(serviceReference.toString().equals("[ihelloservice.HelloService]")) {
+				System.out.println(serviceReference.toString());
+				HelloService h = bundleContext.getService(serviceReference);
+				System.out.println(h.getHello());
+				bundleContext.ungetService(serviceReference);
+			}
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
